303ac9246a9b68ef1c0f45c7c5374f55d4738228
Nikita | private <nlavrr@gmail.com>
2016-10-11 21:59:26 +0300

    tableView | openViewc init

---
+++ tableView/tableView/OpenViewC.swift
@@ -0,0 +1,22 @@
+//
+//  OpenViewC.swift
+//  tableView
+//
+//  Created by Nikita Lavrihins on 11/10/16.
+//  Copyright © 2016 lavr. All rights reserved.
+//
+
+import UIKit
+
+class OpenViewC: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var student = "Yest"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
--- tableView/tableView/ViewController.swift
+++ tableView/tableView/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    var students = ["Mārcis", "Vairis", "Vadims", "Silvestrs"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    //awlays use this 2 func for UITableViewDelegate , UITableViewDataSource protocls
+    
+    //1
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return students.count
     }
-
-
+    //2
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.students[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "openSeg", sender: students[indexPath.row])
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let guest = segue.destination as! OpenViewC
+        
+        guest.student = sender as! String
+}
 }
 
