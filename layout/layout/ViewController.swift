e648ced970bc11951099a4a7afa6a70d91e566d1
Nikita | private <nlavrr@gmail.com>
2016-10-23 10:46:50 +0300

    isPrime app

--- layout/layout/ViewController.swift
+++ layout/layout/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnPressed(_ sender: AnyObject) {
+
+        
+        if let userEnteredText = textField.text {
+            let userEnteredInteger = Int(userEnteredText)
+            
+            
+            if let nummber = userEnteredInteger {
+                if nummber % 2 == 0 {
+                    outputLabel.text = "The nummber: \(nummber) is prime!"
+                } else {
+                    outputLabel.text = "The nummber: \(nummber) is not prime!"
+                }
+            } else {
+                outputLabel.text = "Please enter the nummber!"
+            }
+            
+        }
+        
+        
+        
+        
+    }
 
 }
 
